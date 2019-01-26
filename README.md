433a0be493aada64967ad19bfe5ed3991bb9c35d
Matthijs Mud <matthijsmud@gmail.com>
2019-01-26 17:24:27 +0100

    Setup of the development environment.

---
+++ README.md
@@ -0,0 +1,7 @@
+Assignment to learn work with machine learning. In this case the [TensorFlow.js](https://js.tensorflow.org/) framework is used. Any code is written with [TypeScript](https://www.typescriptlang.org/) for its static typing.
+
+The intent is to distinguish between nuts and bolts. Any other categories that could get added should work on the same principle. In that regard it shouldn't be too hard to add others, but this would require to retrain the model.
+
+The code is heavily inspired by "[Training on Images: Recognizing Handwritten Digits with a Convolutional Neural Network](https://js.tensorflow.org/tutorials/mnist.html)", as it does basically the same but with a different dataset and categories.
+
+~~Images in the dataset are rendered 3D models, created with [Blender](https://www.blender.org/). An image size of 28×28 pixels seems to be the standard for machine learning, hence it is also used for those images.~~ (Still needs to be done.)
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+// Mostly following the instructions in the documentation of webpack.
+// https://webpack.js.org/guides/typescript/#src/components/Sidebar/Sidebar.jsx
+
+var path = require("path");
+var typedoc = require("typedoc-webpack-plugin");
+
+module.exports = {
+	// Specify all different entry points that should get compiled to a separate
+	// output file.
+	entry: {
+		learn: "./src/learn.ts"
+	},
+
+	output: {
+		// Unlike input, output path needs to be absolute... for some reason.
+		path: path.resolve(__dirname, "built"),
+		// The "[name]" part gets substituted by each of the entry points.
+		filename:"[name].js",
+		// Makes it so the output can be used
+		libraryTarget: "umd"
+		// The option "name" is deliberately not used to make the resulting amd
+		// module anonymous.
+	},
+
+	// Enables certain features that make it easier to debug the code, while also
+	// disabling optimizations that would make it harder to debug.
+	mode: "development",
+	// Maps symbols in the output file to the source files, so runtime errors show
+	// the correct line.
+	devtool: "source-map",
+
+	// Certain dependencies of the project don't need to be included in the build.
+	// This typically applies for libraries that are already built, and might be
+	// shared (or cached). This keeps the output file smaller.
+	externals: [ // Array to allow different methods to specify externals.
+		{
+			// Tensorflow is
+			"@tensorflow/tfjs": {
+				// commonjs(2) use the node dependencies.
+				commonjs: "@tensorflow/tfjs",
+				commonjs2: "@tensorflow/tfjs",
+				// AMD uses the filename, and can be mapped.
+				amd: "tensorflow",
+				// Name under which dependency is available on the global object.
+				root: "tf"
+			}
+		}
+	],
+	module: {
+		rules: [
+			{
+				// If a file is provided with either [.ts] or [.tsx] as extension,
+				// use the [ts-loader] to parse the contents of the file.
+				test: /\.tsx?/,
+				use: "ts-loader",
+				// Contents of any of the [node_modules] should remain there; the entire
+				// point is that code doesn't get duplicated.
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		// Webpack only looks at JavaScript files by default. This tells it to
+		// look for other file extensions as well. There needs to be a [module]
+		// that can handle the given extension.
+		extensions: [".tsx", ".ts", ".js"]
+	},
+	plugins: [
+		// Generates documentation for the project.
+		new typedoc({
+			// For all options, see the following link.
+			// https://github.com/TypeStrong/typedoc#arguments
+
+			// Specifies whether the members of all files get clumped together
+			// ("file"), or are kept separate ("modules").
+			mode: "modules",
+			 // Places the docs folder at the project root.
+			out: "../docs"
+		})
+	]
+}
