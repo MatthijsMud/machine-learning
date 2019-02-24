c7b5b886ee282a6c8976978f5fef9709fc8ef467
Matthijs Mud <matthijsmud@gmail.com>
2019-02-24 12:15:12 +0100

    Webpage that serves as an interface.

--- built/learn.js
+++ built/learn.js
@@ -96,13 +96,237 @@ return /******/ (function(modules) { // webpackBootstrap
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/dataset.ts":
+/*!************************!*\
+  !*** ./src/dataset.ts ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+/**
+ * Copyright 2019 Matthijs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @summary Indicates in which state the dataset is. Certain actions are only
+ * available in a given state.
+ */
+var State;
+(function (State) {
+    /**
+     * @summary Indicates the dataset is being initialized.
+     * @description While initializing a dataset, labels can be added to certain
+     * images.
+     */
+    State[State["initializing"] = 0] = "initializing";
+    /**
+     *
+     */
+    State[State["loading"] = 1] = "loading";
+    /**
+     *
+     */
+    State[State["complete"] = 2] = "complete";
+})(State || (State = {}));
+;
+/**
+ *
+ */
+var DataSet = /** @class */ (function () {
+    /**
+     * @param width
+     * @param height
+     */
+    function DataSet(width, height) {
+        this._state = State.initializing;
+        this._images = {};
+        this._width = width;
+        this._height = height;
+    }
+    /**
+     * @summary Adds the specified data to the set.
+     * @throws
+     */
+    DataSet.prototype.add = function (label, urls) {
+        var _this = this;
+        if (_this._state == State.initializing) {
+            urls.forEach(function (url) {
+                if (!(url in _this._images)) {
+                    _this._images[url] = { labels: new Set(), image: null };
+                }
+                _this._images[url].labels.add(label);
+            });
+            // Allow for chaining calls.
+            return _this;
+        }
+        throw new TypeError("Data can only be added when initializing.");
+    };
+    /**
+     * @summary Loads a dataset
+     * @description Causes the given dataset to enter the loading state
+     * (`State.loading`). No more data can be added to the dataset at this point.
+     */
+    DataSet.prototype.load = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _this, promises, key;
+            return __generator(this, function (_a) {
+                _this = this;
+                if (this._state == State.initializing) {
+                    this._state = State.loading;
+                    promises = [];
+                    for (key in this._images) {
+                        promises[promises.length] = loadImage(key).then(function (result) {
+                            var image = result.image;
+                            // TODO: Perform checks to make sure the size of the image is correct.
+                            if (!(image.width === _this._width && image.height === _this._height)) {
+                                throw new TypeError("Image dimensions do not match the specifications.");
+                            }
+                            _this._images[result.key].image = image;
+                            return image;
+                        });
+                    }
+                    return [2 /*return*/, Promise.all(promises).then(function () {
+                            _this._state = State.complete;
+                            return _this;
+                        })];
+                }
+                throw 1;
+            });
+        });
+    };
+    return DataSet;
+}());
+exports.default = DataSet;
+function loadImage(url) {
+    return new Promise(function (resolve, reject) {
+        var image = new Image();
+        image.src = url;
+        image.addEventListener("load", function () {
+            resolve({ image: image, key: url });
+        });
+        image.addEventListener("error", function (e) {
+            console.error("Could not load " + url);
+            reject(e.error);
+        });
+    });
+}
+
+
+/***/ }),
+
 /***/ "./src/learn.ts":
 /*!**********************!*\
   !*** ./src/learn.ts ***!
   \**********************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+
+/**
+ * Copyright 2019 Matthijs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var dataset_1 = __webpack_require__(/*! ./dataset */ "./src/dataset.ts");
+var IMAGE_WIDTH = 56;
+var IMAGE_HEIGHT = 56;
+/**
+ * Objects are being rotated.
+ */
+var FULL_CIRCLE = 360;
+var STEP_SIZE = 5;
+new dataset_1.default(IMAGE_WIDTH, IMAGE_HEIGHT)
+    .add("nut", PathGenerator("data/nut_sideways", FULL_CIRCLE / STEP_SIZE))
+    .add("nut", PathGenerator("data/nut_top", FULL_CIRCLE / STEP_SIZE))
+    .add("nut", PathGenerator("data/nut_bottom", FULL_CIRCLE / STEP_SIZE))
+    .add("bolt", PathGenerator("data/bolt_sideways", FULL_CIRCLE / STEP_SIZE))
+    .add("bolt", PathGenerator("data/bolt_top", FULL_CIRCLE / STEP_SIZE))
+    .add("bolt", PathGenerator("data/bolt_bottom", FULL_CIRCLE / STEP_SIZE))
+    .load().then(function (dataset) {
+    console.log(dataset);
+    var finished = document.createElement("div");
+    finished.innerHTML = "Finished";
+    document.body.appendChild(finished);
+})
+    .catch(function (reason) {
+    console.error(reason);
+});
+/**
+ * @summary Generates a number of urls based on the parameters.
+ * @description The path is generated by appending numbers (padded with `0`) to
+ * the folder location. In addition to a `".png"` extension.
+ *
+ * The number that is appended starts at 1, and is incremented up to (including)
+ * the specified count. For example`"0001.png"`, `"0002.png"`, ..., `"NNNN.png"`.
+ * @param folder Path to the folder that contains the images.
+ * @param count Number of different paths to generate.
+ */
+function PathGenerator(folder, count) {
+    return Array.apply(null, { length: count }).map(function (_, index) {
+        return folder + "/" + (Array(5).join("0") + (1 + index)).slice(-4) + ".png";
+    });
+}
 
 
 /***/ })
